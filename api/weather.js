6bbcc080d0f8c6cea888c7704f4a16ad84c1e91e
CypherHarsh <167523521+CypherHarsh@users.noreply.github.com>
2024-10-07 11:34:43 +0530

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,20 +1,18 @@
-// api/weather.js
 export default async function handler(req, res) {
-    const { city } = req.query;
-    const apiKey = process.env.API_KEY; // Your API key
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const { city } = req.query;
+  const apiKey = process.env.API_KEY; // Use the environment variable
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
     
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      
-      if (response.ok) {
-        res.status(200).json(data);
-      } else {
-        res.status(response.status).json({ error: data.message });
-      }
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to fetch weather data' });
+    if (response.ok) {
+      res.status(200).json(data);
+    } else {
+      res.status(response.status).json({ error: data.message });
     }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch weather data' });
   }
-  
+}
